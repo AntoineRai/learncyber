c973946d28d90546879c0f587ce4f7e207897795
thomas <cornu.thomas6@gmail.com>
2024-04-24 10:49:23 +0200

    feat (global) : création composant présentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Presentation from "@/components/presentation";
+
 export default function Home() {
   return (
     <main className="h-full flex flex-col items-center justify-center">
-
+      <Presentation/>
     </main>
   );
 }
